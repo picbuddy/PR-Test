9efc024467b177c8e5b7b8e4421c86e769347575
Keyrock2 <mbrock1047@gmail.com>
2025-09-30 19:20:26 +0900

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -9,5 +9,6 @@ public class Main {
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
             System.out.println("i = " + i);
         }
+        System.out.println("안녕하세요 ~~ 수업 끝내주세용");
     }
 }
